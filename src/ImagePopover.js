1e641448844584596e5995397fc4528021972baf
Patrick Lu <lightning.lu7@gmail.com>
2017-07-19 22:56:54 -0700

    99% width so the border lives. seems like a bug and something we need to fix

--- src/ImagePopover.js
+++ src/ImagePopover.js
@@ -16,7 +16,7 @@ import { smallIcon, mediumIcon, largeIcon } from './icons/svg';
 const ICONS = [
   {id: 'small', icon: smallIcon, width: '50%'},
   {id: 'medium', icon: mediumIcon, width: '75%'},
-  {id: 'large', icon: largeIcon, width: '100%'},
+  {id: 'large', icon: largeIcon, width: '99%'},
 ]
 
 export default class ImageComponent extends Component {
